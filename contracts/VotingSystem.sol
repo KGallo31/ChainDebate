49697ffee4d096f8b6dccf9cd0c97a8655c123a6
KGallo31 <Gallok731@gmail.com>
2025-01-01 23:42:04 -0500

    Removes comments

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -2,35 +2,32 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
-contract VotingSystem is ReentrancyGuard, Initializable {
+contract VotingSystem is ReentrancyGuard {
     
     // Struct to represent a Voter's details in a specific session
     struct Voter {
-        address userID;
         bool hasVoted;      // To check if the voter has already voted
         uint32 vote;          // The ID of the topic the voter voted for
-        bytes32[] votedForTopics;
     }
 
     // Struct to represent a Topic for voting in each session
     struct Topic {
-        bytes32 id;            // ID of the topic
+        uint32 id;            // ID of the topic
         string description; // Description of the topic
         uint32 voteCount;     // Number of votes for the topic
     }
 
     // Struct to represent a voting session
     struct VotingSession {
-        string title;
-        uint256 startTime;
-        uint256 endTime;
-        Voter[] votedUsers; // Array of users that have voted
-        Topic[] votingTopics; // Araay of topics to be voted
+        string title;       
+        uint256 startTime;     
+        uint256 endTime;       
+        mapping(address => Voter) voters;  // Mapping of voters in the session
+        mapping(uint32 => Topic) topics;     // Mapping of topics in the session
         address creator;    // The address of the user who created the session
-        uint32 totalVotes;
+        uint32 totalVotes;    
     }
 
     struct VotingSessionInfo {
@@ -43,8 +40,6 @@ contract VotingSystem is ReentrancyGuard, Initializable {
         bool isActive;
     }
 
-    event VotingSessionCreated(bytes32 sessionId, string title, uint256 startTime, uint256 endTime, address creator, uint32 totalVotes);
-
     // Mapping from user to the last session they created and the timestamp
     mapping(address => uint256) public lastSessionCreationTime;
     // Mapping from session ID to the VotingSession struct
@@ -52,10 +47,7 @@ contract VotingSystem is ReentrancyGuard, Initializable {
     // Counter to keep track of the total number of voting sessions created
     uint32 public totalSessions;
 
-    address public owner;
-
-    function initialize(address _owner, uint32 _totalSession, address[] usersLastCreationTimeAddress, uint256[] usersLastCreationTime, bytes32[] votingSessionKeys, VotingSession[] _votingSessions) public initializer {
-    }
+    event VotingSessionCreated(bytes32 sessionId, string title, uint256 startTime, uint256 endTime, address creator, uint32 totalVotes);
 
     // Modifier to check if the user can create a session (once every 24 hours)
     modifier canCreateSession() {
@@ -72,6 +64,11 @@ contract VotingSystem is ReentrancyGuard, Initializable {
         _;
     }
 
+     // Function to check if a topic is the default value
+    function isTopicDefault(Topic memory topic) internal pure returns (bool) {
+        return topic.id == 0 && keccak256(bytes(topic.description)) == keccak256(bytes("")) && topic.voteCount == 0;
+    }
+
     // Function to create a new voting session
     function createVotingSession(uint32 _votingDurationInMinutes, string[] memory _topicDescriptions, string memory _title) public canCreateSession returns (bytes32) {
         // Generate a unique session ID using keccak256 (combining creator address, timestamp, and topics)
@@ -79,9 +76,9 @@ contract VotingSystem is ReentrancyGuard, Initializable {
 
         VotingSession storage newSession = votingSessions[sessionId];  // Create a new voting session
         newSession.title = _title;
-        newSession.startTime = block.timestamp;  // Set the start time
-        newSession.endTime = block.timestamp + (_votingDurationInMinutes * 1 minutes);  // Set the end time
-        newSession.creator = msg.sender;  // Set the creator of the session
+        newSession.startTime = block.timestamp; 
+        newSession.endTime = block.timestamp + (_votingDurationInMinutes * 1 minutes); 
+        newSession.creator = msg.sender;  
 
         // Add topics to the voting session
         for (uint32 i = 0; i < _topicDescriptions.length; i++) {
@@ -131,7 +128,6 @@ contract VotingSystem is ReentrancyGuard, Initializable {
         Topic storage selectedTopic = session.topics[_topicId];
         selectedTopic.voteCount += 1;
         
-        // Update total votes
         session.totalVotes += 1;
     }
 
@@ -146,15 +142,6 @@ contract VotingSystem is ReentrancyGuard, Initializable {
         Topic memory topic = votingSessions[_sessionId].topics[_topicId];  // Fetch the topic in the session
         return topic;
     }
-
-    // Function to check how much time is left in a specific voting session
-    // function votingTimeLeft(bytes32 _sessionId) public nonReentrant returns (uint256) {
-    //     if (block.timestamp >= votingSessions[_sessionId].endTime) {
-    //         return 0;  // If the current time is past the voting end time, return 0
-    //     } else {
-    //         return votingSessions[_sessionId].endTime - block.timestamp;  // Return the remaining time
-    //     }
-    // }
 
     // Function to determine which topic received the most votes in a session
     function winner(bytes32 _sessionId) public view returns (string memory winningTopicDescription) {
@@ -171,11 +158,6 @@ contract VotingSystem is ReentrancyGuard, Initializable {
             topic = session.topics[topic.id + 1];
         }
 
-        return winningTopic;  // Return the description of the topic with the most votes
-    }
-
-    // Function to check if a topic is the default value
-    function isTopicDefault(Topic memory topic) internal pure returns (bool) {
-        return topic.id == 0 && keccak256(bytes(topic.description)) == keccak256(bytes("")) && topic.voteCount == 0;
+        return winningTopic; 
     }
 }
